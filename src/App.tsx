adc98b9fd934734184708c57fc62c5cfc3874de6
Lucia <slowlyxixi@outlook.com>
2024-10-31 10:25:42 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import RouterApp from "@/pages/App/RouterApp";
+import DebugScreensWrapper from "@/pages/App/DebugScreensWrapper";
+
+function App() {
+  return (
+    <DebugScreensWrapper>
+      <RouterApp />
+    </DebugScreensWrapper>
+  );
+}
+
+export default App;
